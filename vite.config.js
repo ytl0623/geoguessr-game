cfe0362eac4ccc6c62e1384202b2b3ce18d29bbd
ytl0623 <david89062388@gmail.com>
2026-07-16 19:41:37 +0800

    fix base //
    Signed-off-by: ytl0623 <david89062388@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: /geoguessr-game/,
+  base: '/geoguessr-game/',
   plugins: [react()],
   define: {
     __BUILD_TIME__: JSON.stringify(
